63c3155aff76fc9df66c1fb576711ed00cc95eb3
gaohuizhang <41465240+gaohuizhang@users.noreply.github.com>
2020-01-16 14:45:45 +0800

    change

---
+++ js/typeme.js
@@ -0,0 +1,9 @@
+var typed = $(".typed");
+
+$(() => {
+	typed.typed({
+	  strings: ["programming.", "playing basketball", "reading.",  "running.",  "studying math.", "playing table-tennis."],
+	  typeSpeed: 140,
+	  loop: true,
+	});
+});
